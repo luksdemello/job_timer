1939cd38891bd79f9d569876cbbd9debb3065714
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-11 15:41:08 -0300

    feat: config home and login module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/home/home_module.dart';
+import 'package:job_timer/app/modules/login/login_module.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
 
 class AppModule extends Module {
@@ -8,5 +10,7 @@ class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
         ChildRoute('/', child: (context, args) => const SplashPage()),
+        ModuleRoute('/login', module: LoginModule()),
+        ModuleRoute('/home', module: HomeModule()),
       ];
 }
