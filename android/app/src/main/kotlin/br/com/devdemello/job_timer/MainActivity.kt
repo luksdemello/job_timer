1287f566189a82f7901f183d19da3861d5c3d39a
Lucas de mello <lucas_mello6@hotmail.com>
2023-06-11 15:31:23 -0300

    chore: config project with dependencies, icons and structure

---
+++ android/app/src/main/kotlin/br/com/devdemello/job_timer/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.devdemello.job_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/core/ui/job_timer_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class JobTimer {
+  JobTimer._();
+
+  static const _kFontFam = 'JobTimer';
+  static const String? _kFontPkg = null;
+
+  static const IconData okCircled2 =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData angleDoubleRight =
+      IconData(0xf101, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
